b95ef787af48772136e14e087e1ca37e4c939c29
Imzan Khan <imzan@mindtrades.com>
2019-11-09 14:27:57 -0500

    Update routing for https

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,27 @@
 <?php
+Route::group(['scheme' => 'https'], function () {
+    Route::get('/', function () {
+        return view('vue');
+    });
 
-Route::get('/', function () {
-    return view('vue');
+    Route::get('/home', function () {
+        return view('vue');
+    });
+
+    Route::get('/unit/add', function () {
+        return view('vue');
+    })->middleware('auth');
+
+    Route::get('/profile', function () {
+        return view('vue');
+    })->middleware('auth');
+
+    Route::get('/home', function () {
+        return redirect('/');
+    });
+
+    Auth::routes();
+
+    Route::get('/logout', 'Auth\LoginController@logout');
+    Route::get('/{any}', 'VueController@index')->where('any', '.*');
 });
-
-Route::get('/home', function () {
-    return view('vue');
-});
-
-Route::get('/unit/add', function () {
-    return view('vue');
-})->middleware('auth');
-
-Route::get('/profile', function () {
-    return view('vue');
-})->middleware('auth');
-
-Route::get('/home', function () {
-    return redirect('/');
-});
-
-Auth::routes();
-
-Route::get('/logout', 'Auth\LoginController@logout');
-Route::get('/{any}', 'VueController@index')->where('any', '.*');
