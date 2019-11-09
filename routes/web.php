f6d3db1c58a33df8c3f6bd4bdf9dbf791595ce3f
Imzan Khan <imzan@mindtrades.com>
2019-11-09 14:29:11 -0500

    Remove https

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,25 @@
 <?php
-Route::group(['scheme' => 'https'], function () {
-    Route::get('/', function () {
-        return view('vue');
-    });
-
-    Route::get('/home', function () {
-        return view('vue');
-    });
-
-    Route::get('/unit/add', function () {
-        return view('vue');
-    })->middleware('auth');
-
-    Route::get('/profile', function () {
-        return view('vue');
-    })->middleware('auth');
-
-    Route::get('/home', function () {
-        return redirect('/');
-    });
-
-    Auth::routes();
-
-    Route::get('/logout', 'Auth\LoginController@logout');
-    Route::get('/{any}', 'VueController@index')->where('any', '.*');
+Route::get('/', function () {
+    return view('vue');
 });
+
+Route::get('/home', function () {
+    return view('vue');
+});
+
+Route::get('/unit/add', function () {
+    return view('vue');
+})->middleware('auth');
+
+Route::get('/profile', function () {
+    return view('vue');
+})->middleware('auth');
+
+Route::get('/home', function () {
+    return redirect('/');
+});
+
+Auth::routes();
+
+Route::get('/logout', 'Auth\LoginController@logout');
+Route::get('/{any}', 'VueController@index')->where('any', '.*');
