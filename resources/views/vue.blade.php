d1b46eb9424558cdc301fad8b0428fb2bd2f0597
Imzan Khan <imzan@mindtrades.com>
2019-11-09 22:44:46 -0500

    Add google analytics script

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -16,6 +16,19 @@
         }
     </script>
     <script data-ad-client="ca-pub-2185131142940584" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151917177-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-151917177-1');
+    </script>
+
 </head>
 
 <body>
