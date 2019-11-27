5eeba63b64b59c5259f4005bd076dd53ef5d769c
Imzan Khan <imzan@mindtrades.com>
2019-11-26 22:22:08 -0500

    Add advanced search functionality, and update UI

--- resources/js/components/Units/Units.vue
+++ resources/js/components/Units/Units.vue
@@ -1,14 +1,264 @@
 <template>
     <div>
         <h1 class="mb-4 mt-3">Unit Listings</h1>
-        <div class="col-md-8 col-sm-12 my-5 mx-0 mx-auto">
+        <div class="col-md-8 col-sm-12 mt-5 mb-3 mx-0 mx-auto">
             <input
                 class="form-control"
                 type="text"
                 v-model="search"
-                placeholder="Search Units..."
+                placeholder="Search Units or Usernames..."
                 v-on:keydown.enter.prevent
             />
+        </div>
+        <div>
+            <b-button v-b-toggle.advancedSearch variant="btn btn-link"
+                >+ Advanced Search</b-button
+            >
+            <b-collapse id="advancedSearch">
+                <hr />
+                <p class="searchHeader">
+                    Hide/Show Columns:
+                </p>
+                <div class="row">
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="element_weapon"
+                            type="checkbox"
+                            @change="show_hide('element_weapon')"
+                            checked
+                        />
+                        Show Weapon Element</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="hp"
+                            type="checkbox"
+                            @change="show_hide('hp')"
+                            checked
+                        />
+                        Show HP</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="mp"
+                            type="checkbox"
+                            @change="show_hide('mp')"
+                            checked
+                        />
+                        Show MP</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="atk"
+                            type="checkbox"
+                            @change="show_hide('atk')"
+                            checked
+                        />
+                        Show ATK</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="mag"
+                            type="checkbox"
+                            @change="show_hide('mag')"
+                            checked
+                        />
+                        Show MAG</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="def"
+                            type="checkbox"
+                            @change="show_hide('def')"
+                            checked
+                        />
+                        Show DEF</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="spr"
+                            type="checkbox"
+                            @change="show_hide('spr')"
+                            checked
+                        />
+                        Show SPR</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="pevade"
+                            type="checkbox"
+                            @change="show_hide('pevade')"
+                        />
+                        Show Physical Evade</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="lb_damage"
+                            type="checkbox"
+                            @change="show_hide('lb_damage')"
+                        />
+                        Show LB Damage</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="magkillers"
+                            type="checkbox"
+                            @change="show_hide('magkillers')"
+                        />
+                        Show Killers</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="status"
+                            type="checkbox"
+                            @change="show_hide('status')"
+                        />
+                        Show Status Resists</label
+                    >
+                    <label class="col-md-2 col-sm-4 col-6"
+                        ><input
+                            class="elemental"
+                            type="checkbox"
+                            @change="show_hide('elemental')"
+                        />
+                        Show Element Resists</label
+                    >
+                </div>
+                <hr />
+                <p class="searchHeader">
+                    Stat Restrictions:
+                </p>
+                <div class="row mt-2">
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <label for="range-hp"
+                            >HP greater than: {{ hpLimit }}</label
+                        >
+                        <b-form-input
+                            class="mt-2"
+                            id="range-hp"
+                            v-model.number="hpLimit"
+                            type="range"
+                            min="0"
+                            step="1000"
+                            max="30000"
+                        ></b-form-input>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <label for="range-mp"
+                            >MP greater than: {{ mpLimit }}</label
+                        >
+                        <b-form-input
+                            class="mt-2"
+                            id="range-mp"
+                            v-model.number="mpLimit"
+                            type="range"
+                            min="0"
+                            step="100"
+                            max="1300"
+                        ></b-form-input>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <label for="range-atk"
+                            >Attack greater than: {{ atkLimit }}</label
+                        >
+                        <b-form-input
+                            class="mt-2"
+                            id="range-atk"
+                            v-model.number="atkLimit"
+                            type="range"
+                            min="0"
+                            step="250"
+                            max="3500"
+                        ></b-form-input>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <label for="range-mag"
+                            >Magic greater than: {{ magLimit }}</label
+                        >
+                        <b-form-input
+                            class="mt-2"
+                            id="range-mag"
+                            v-model.number="magLimit"
+                            type="range"
+                            min="0"
+                            step="250"
+                            max="3250"
+                        ></b-form-input>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <label for="range-def"
+                            >Defense greater than: {{ defLimit }}</label
+                        >
+                        <b-form-input
+                            class="mt-2"
+                            id="range-def"
+                            v-model.number="defLimit"
+                            type="range"
+                            min="0"
+                            step="250"
+                            max="1750"
+                        ></b-form-input>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <label for="range-spr"
+                            >Spirit greater than: {{ sprLimit }}</label
+                        >
+                        <b-form-input
+                            class="mt-2"
+                            id="range-spr"
+                            v-model.number="sprLimit"
+                            type="range"
+                            min="0"
+                            step="250"
+                            max="2500"
+                        ></b-form-input>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <label for="range-limit"
+                            >LB greater than: {{ lbLimit }}</label
+                        >
+                        <b-form-input
+                            class="mt-2"
+                            id="range-limit"
+                            v-model.number="lbLimit"
+                            type="range"
+                            min="0"
+                            step="50"
+                            max="200"
+                        ></b-form-input>
+                    </div>
+                    <div class="col-md-3 col-sm-6 col-12">
+                        <label for="range-limit"
+                            >Evade greater than: {{ evadeLimit }}</label
+                        >
+                        <b-form-input
+                            class="mt-2"
+                            id="range-limit"
+                            v-model.number="evadeLimit"
+                            type="range"
+                            min="0"
+                            step="25"
+                            max="100"
+                        ></b-form-input>
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="mx-auto mx-0">
+                        <b-button
+                            v-b-toggle.advancedSearch
+                            variant="btn btn-link"
+                            >Close Search</b-button
+                        >
+                        <button
+                            class="btn btn-outline-danger"
+                            @click="resetFilters"
+                        >
+                            Reset Filters
+                        </button>
+                    </div>
+                </div>
+                <hr />
+            </b-collapse>
         </div>
         <div class="d-md-flex float-right mb-4">
             <b-pagination
@@ -43,6 +293,9 @@
                     </a>
                 </p>
                 <p>Slot: {{ data.item.slot }}</p>
+            </template>
+
+            <template v-slot:cell(profile)="data">
                 <p class="mb-0">
                     {{ data.item.profile.ign }}: <br />
                     {{ data.item.profile.friendCode | friendCode }}
@@ -82,7 +335,7 @@
             </template>
 
             <template v-slot:cell(lb_damage)="data">
-                {{ data.item.lb_damage === '' ? 0 : data.item.lb_damage }}%
+                {{ data.item.lb_damage === "" ? 0 : data.item.lb_damage }}%
             </template>
 
             <template v-slot:cell(magkillers)="data">
@@ -154,6 +407,18 @@
                 </div>
             </template>
         </b-table>
+        <div class="d-md-flex float-right mb-4">
+            <b-pagination
+                v-model="currentPage"
+                :total-rows="rows"
+                :per-page="perPage"
+                aria-controls="unitsTable"
+                first-text="First"
+                prev-text="Prev"
+                next-text="Next"
+                last-text="Last"
+            ></b-pagination>
+        </div>
     </div>
 </template>
 
@@ -166,6 +431,15 @@ export default {
             perPage: 5,
             currentPage: 1,
             search: "",
+            hpLimit: 0,
+            mpLimit: 0,
+            atkLimit: 0,
+            defLimit: 0,
+            magLimit: 0,
+            sprLimit: 0,
+            lbLimit: 0,
+            evadeLimit: 0,
+            ribbon: false,
             fields: [
                 {
                     key: "name",
@@ -173,68 +447,101 @@ export default {
                     sortable: true
                 },
                 {
+                    key: "profile",
+                    label: "Profile",
+                    sortable: false
+                },
+                {
                     key: "element_weapon",
                     label: "Element(s)",
-                    sortable: true
+                    sortable: true,
+                    thClass: "",
+                    tdClass: ""
                 },
                 {
                     key: "hp",
                     label: "HP",
-                    sortable: true
+                    sortable: true,
+                    thClass: "",
+                    tdClass: ""
                 },
                 {
                     key: "mp",
                     label: "MP",
-                    sortable: true
+                    sortable: true,
+                    thClass: "",
+                    tdClass: ""
                 },
                 {
                     key: "atk",
                     label: "ATK",
-                    sortable: true
+                    sortable: true,
+                    thClass: "",
+                    tdClass: ""
                 },
                 {
                     key: "mag",
                     label: "MAG",
-                    sortable: true
+                    sortable: true,
+                    thClass: "",
+                    tdClass: ""
                 },
                 {
                     key: "def",
                     label: "DEF",
-                    sortable: true
+                    sortable: true,
+                    thClass: "",
+                    tdClass: ""
                 },
                 {
                     key: "spr",
                     label: "SPR",
-                    sortable: true
+                    sortable: true,
+                    thClass: "",
+                    tdClass: ""
                 },
                 {
                     key: "pevade",
                     label: "P.Evade",
-                    sortable: true
+                    sortable: true,
+                    thClass: "d-none",
+                    tdClass: "d-none",
+                    defaultCol: false
                 },
                 {
                     key: "lb_damage",
                     label: "LB",
-                    sortable: true
+                    sortable: true,
+                    thClass: "d-none",
+                    tdClass: "d-none",
+                    defaultCol: false
                 },
                 {
                     key: "magkillers",
                     label: "Killers",
                     sortable: false,
-                    colspan: 3
+                    colspan: 3,
+                    thClass: "d-none",
+                    tdClass: "d-none",
+                    defaultCol: false
                 },
                 {
                     key: "status",
                     label: "Status",
-                    sortable: false
+                    sortable: false,
+                    thClass: "d-none",
+                    tdClass: "d-none",
+                    defaultCol: false
                 },
                 {
                     key: "elemental",
                     label: "Elemental",
-                    sortable: false
+                    sortable: false,
+                    thClass: "d-none",
+                    tdClass: "d-none",
+                    defaultCol: false
                 }
             ],
-
             link: "https://ffbeEquip.com/builder.html?server=GL#",
             units: [],
             isLoading: true
@@ -261,11 +568,68 @@ export default {
     computed: {
         filterUnits() {
             return this.units.filter(unit => {
-                return unit.name.toLowerCase().match(this.search.toLowerCase());
+                let unitName = unit.name
+                    .toLowerCase()
+                    .match(this.search.toLowerCase());
+                let inGameName = unit.profile.ign
+                    .toLowerCase()
+                    .match(this.search.toLowerCase());
+                let attackLimit = unit.atk >= this.atkLimit;
+                let magicLimit = unit.mag >= this.magLimit;
+                let defenseLimit = unit.def >= this.defLimit;
+                let spiritLimit = unit.spr >= this.sprLimit;
+                let healthLimit = unit.hp >= this.hpLimit;
+                let manaLimit = unit.mp >= this.mpLimit;
+                let limitLimit = unit.lb_damage >= this.lbLimit;
+                let evadeLimit = unit.pevade >= this.evadeLimit;
+
+                return (
+                    (unitName || inGameName) &&
+                    attackLimit &&
+                    magicLimit &&
+                    defenseLimit &&
+                    spiritLimit &&
+                    healthLimit &&
+                    manaLimit &&
+                    evadeLimit &&
+                    limitLimit
+                );
             });
         },
         rows() {
             return this.filterUnits.length;
+        }
+    },
+    methods: {
+        show_hide(colName) {
+            this.fields.filter(col => {
+                if (col.key === colName) {
+                    if (col.thClass === "" && col.tdClass === "") {
+                        col.thClass = "d-none";
+                        col.tdClass = "d-none";
+                    } else {
+                        col.thClass = "";
+                        col.tdClass = "";
+                    }
+                }
+            });
+        },
+        resetFilters() {
+            this.fields.forEach(col => {
+                if (col.defaultCol === false) {
+                    document.querySelector("." + col.key).checked = false;
+                    col.thClass = "d-none";
+                    col.tdClass = "d-none";
+                }
+            });
+            this.hpLimit = 0;
+            this.mpLimit = 0;
+            this.atkLimit = 0;
+            this.defLimit = 0;
+            this.magLimit = 0;
+            this.sprLimit = 0;
+            this.lbLimit = 0;
+            this.evadeLimit = 0;
         }
     }
 };
@@ -292,5 +656,9 @@ li {
 
 .killers {
     width: 120px;
+}
+
+.searchHeader {
+    font-weight: bold;
 }
 </style>
